1beaee51e9c4f8d9b66103d9864dbdeb060c49c9
Danny Dyla <dyladan@gmail.com>
2014-04-01 16:23:54 -0400

    mobile header persistent when menu closed but relative when open

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,18 +2,23 @@ function makeMenuVisible() {
   document.getElementById("menu").style.display = "block";
   document.getElementById("menu").style.position = "relative";
   document.getElementById("hamburger").setAttribute("onclick", "makeMenuInvisible()");
+  document.getElementById("mheader").style.position = "relative";
+  document.getElementById("layout").style.paddingTop = "0px";
 }
 
 function makeMenuInvisible() {
   document.getElementById("menu").style.display = "none";
   document.getElementById("menu").style.position = "absolute";
   document.getElementById("hamburger").setAttribute("onclick", "makeMenuVisible()");
+  document.getElementById("mheader").removeAttribute("style");
+  document.getElementById("layout").removeAttribute("style");
 }
 
 function fixMenu() {
   var w = document.body.clientWidth;
 
   if (w >= 832) {
+    makeMenuInvisible();
     document.getElementById("menu").removeAttribute("style");
     document.getElementById("hamburger").setAttribute("onclick", "makeMenuVisible()");
   }
